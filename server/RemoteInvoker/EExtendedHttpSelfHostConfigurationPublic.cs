81b050e89f91d9ca9fc9481512756fc3f56f010b
Robbert van Leeuwen <rleeuwen@dsea.nl>
2026-01-03 13:20:36 +0100

    Refactoring server code

---
+++ server/RemoteInvoker/EExtendedHttpSelfHostConfigurationPublic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ServiceModel;
+using System.Web.Http.SelfHost;
+using System.ServiceModel.Channels;
+using System.Web.Http.SelfHost.Channels;
+
+internal class EExtendHttpSelfHostConfigurationpublic : HttpSelfHostConfiguration
+{
+    public EExtendHttpSelfHostConfigurationpublic(Uri baseAddress) : base(baseAddress)
+    {
+    }
+    protected override BindingParameterCollection OnConfigureBinding(HttpBinding httpBinding)
+    {
+        httpBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Certificate;
+        httpBinding.Security.Mode = HttpBindingSecurityMode.Transport;
+        httpBinding.MaxReceivedMessageSize = long.MaxValue;
+        this.MaxBufferSize = int.MaxValue;
+        this.MaxReceivedMessageSize = long.MaxValue;
+
+        return base.OnConfigureBinding(httpBinding);
+    }
+}
